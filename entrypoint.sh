7b2f895188e0866a50344ee726c84b71c5cbd888
Rostislav Korin <rostislavkorin@Rostislavs-MacBook-Pro.local>
2019-10-28 12:12:56 +0200

    [Feature] script

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set +e 
+
+if [[ -z "${BACKUP_CRON_SCHEDULE}" ]]; then
+  /usr/local/bin/backup
+else
+  BACKUP_CRON_SCHEDULE=${BACKUP_CRON_SCHEDULE}
+  echo "${BACKUP_CRON_SCHEDULE} /usr/local/bin/backup" > /etc/crontabs/root
+  echo "Starting cron ${BACKUP_CRON_SCHEDULE}" 
+  crond -f -d 0 
+fi
